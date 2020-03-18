7f184312373cbc934331698093d843c076d0b5bf
unknown <mhammad1991h@gmail.com>
2020-03-18 15:20:18 +0100

    opdracht 5

--- toevoegen-nummers.php
+++ toevoegen-nummers.php
@@ -1,5 +1,5 @@
 <?php
-	include"database.php";
+include_once('database.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,28 +11,35 @@
 	<body>
 			
 				
-			
+						<?php
+if(isset($_POST["submit"])){
+$idsong=$_POST['idz'];
+$duur=$_POST['oms'];
+$titel=$_POST['slo'];
+if($idsong && $duur && $titel ){
+$insert=mysqli_query($db_conc," insert into song
+(idsong,duur,titel)
+VALUES
+('$idsong','$duur','$titel')
+ ");
+
+if($insert){
+	echo "<div class='success'>Insert Success..</div>";
+}
+
+}else{
+	echo "<div class='error'>Insert Failed..</div>";
+}
+
+}
+						
+?>		
 					
 						<h3 > Add nummers</h3><br>
-						
-					<?php
-						if(isset($_POST["submit"]))
-						{
-							$sq="INSERT INTO `song` (`idsong`, `duur`, `titel`) VALUES('{$_POST["idz"]}','{$_POST["oms"]}','{$_POST["slo"]}')";
-							if($db->query($sq))
-							{
-								echo "<div class='success'>Insert Success..</div>";
-							}
-							else
-							{
-								echo "<div class='error'>Insert Failed..</div>";
-							}
-							
-						}
-						
-					?>
+				
+
 					<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-					     <label>id artiest</label><br>
+					     <label>id nummer</label><br>
 					     <input type="text" name="idz" required class="input">
 					     <br><br>
 					     <label>duur</label><br>
